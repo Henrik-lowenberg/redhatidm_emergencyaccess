45d606cd7d6e0b3108b61954a15df01253ec27b4
Henrik L <henrik.lowenberg@gmail.com>
2021-03-01 19:14:03 +0100

    support for AD users works

--- emergencyAccess.py
+++ emergencyAccess.py
@@ -8,6 +8,8 @@
 #
 # Import the freeipa library
 from python_freeipa import ClientMeta
+# pretty print to parse to human-readable output
+import pprint
 # parse json data
 import json
 # date
@@ -17,17 +19,20 @@ import sys, getopt
 # import regular expressions
 import re
  
+
 def main(argv):
   """
-  Main contains methods: sudruleadd, hbacruleadd
+  Main function for starting methods
   """
   user = None
   servers = None
+  extuser1 = None
+  extuser2 = None
  
   try:
     opts, args = getopt.getopt(argv,"hu:s:",["user=","servers="])
     for opt, arg in opts:
-
+ 
      if opt == '-h':
        print ('emergencyAccess.py -u username -s server1.example.com,server2.example.com')
        sys.exit(3)
@@ -41,33 +46,57 @@ def main(argv):
          res = tuple(servers.split(","))
          servers = res
          #print res
+         #print (servers)
 
-    #   print (servers)
   except getopt.GetoptError:
     print ('emergencyAccess.py -u username -s server1.example.com,server2.example.com')
     sys.exit(2)
-
+ 
   # get date, format 20210101
   now = datetime.now() # current date and time
   todaysdate = now.strftime("%Y%m%d")
-
+ 
   # get login credentials
-  client = ClientMeta("segotl6204.idm.it.hclgss.com")
-  client.login("admin","idmhcl123")
-
+  client = ClientMeta("replica1.idm.example.com")
+  client.login("admin","changeme")
+ 
 # testing regular expressions to identify AD account
   # search for @sds.hclgss.com or similar
   if re.search("\@[a-z]{3,9}\.[a-z]{3,9}\.[a-z]{3,9}", user):
-    extuser = user
-#    print(extuser)
+    extuser1 = user
   # search for SDS\ or similar with raw to include backslash
   elif re.search(r"[A-Z]{3,5}\\", user):
-    extuser = user
-#    print(extuser)
+    extuser2 = user
+ 
 
-  def sudoruleadd(user,servers):
+  if extuser1: print "extuser1: " + extuser1
+  if extuser2: print "extuser2: " + extuser2
+ 
+  if extuser1: # split on @, return left side
+    tmpfriendlyUserName = extuser1.split("@", 1)
+    friendlyUserName = tmpfriendlyUserName[0]
+    extuser = extuser1
+  elif extuser2: # split on \, return right side
+    tmpfriendlyUserName = extuser2.split("\\", 1)
+    friendlyUserName = tmpfriendlyUserName[1]
+    extuser = extuser2
+ 
+ 
+  def groupadd(friendlyUserName):
+    print "In groupadd()"
+    posixgroup_cn = "unix_emerg_" + friendlyUserName + "_" + todaysdate + "_user"
+    externalgroup_cn = "unix_emerg_" + friendlyUserName + "_" + todaysdate + "_user_ad"
+    client.group_add(posixgroup_cn)
+    client.group_add(externalgroup_cn, external=True)
+    client.group_add_member(posixgroup_cn,group=externalgroup_cn)
+    client.group_add_member(externalgroup_cn,ipaexternalmember=extuser)
+    return posixgroup_cn
+ 
+ 
+  def sudoruleadd(user,servers,posixgroup_cn):
+    print "In sudoruleadd()"
     # define common name
-    emerg_sudoruleadd_cn = "unix_emerg_" + user + "_" + todaysdate + "_high_rule"
+    emerg_sudoruleadd_cn = "unix_emerg_" + friendlyUserName + "_" + todaysdate + "_high_rule"
     # show rule if it exists
 #    print client.sudorule_show(emerg_sudoruleadd_cn)
 #    sys.exit()
@@ -76,37 +105,51 @@ def main(argv):
     # add host(s) to rule
     client.sudorule_add_host(emerg_sudoruleadd_cn,host=servers)
     # add local IDM user to rule
-    client.sudorule_add_user(emerg_sudoruleadd_cn,user=user)
-#    if not extuser:
-#      client.sudorule_add_user(emerg_sudoruleadd_cn,user=user)
-#    elif extuser:
-#      client.sudorule_add_user(emerg_sudoruleadd_cn,externaluser=extuser)
+#    client.sudorule_add_user(emerg_sudoruleadd_cn,user=user)
+    if not extuser:
+      client.sudorule_add_user(emerg_sudoruleadd_cn,user=user)
+      print "not extuser"
+    elif extuser:
+      client.sudorule_add_user(emerg_sudoruleadd_cn,group=posixgroup_cn)
+      print "extuser true"
+      print posixgroup_cn
 
     # add command to rule (need to exist already)
 #    client.sudorule_add_allow_command(emerg_sudoruleadd_cn,sudocmd="sudo su -")
-
-
-  def hbacruleadd(user,servers):
+ 
+ 
+  def hbacruleadd(user,servers,posixgroup_cn):
+    print "In hbacruleadd()"
     # define common name
-    emerg_hbacruleadd_cn = "allow_unix_emerg_" + user + "_" + todaysdate + "_high_hbac"
+    emerg_hbacruleadd_cn = "allow_unix_emerg_" + friendlyUserName + "_" + todaysdate + "_high_hbac"
     hbac_descr = "temporary access for user ${user} to server(s): ${servers}"
     # add rule
     client.hbacrule_add(emerg_hbacruleadd_cn)
     # add hosts to rule
-    client.hbacrule_add_host(emerg_hbacruleadd_cn, host=servers, all='True' )
+    client.hbacrule_add_host(emerg_hbacruleadd_cn, host=servers,all='True')
     # add user to rule
-    client.hbacrule_add_user(emerg_hbacruleadd_cn, user=user, all='True' )
+    if not extuser:
+      client.hbacrule_add_user(emerg_hbacruleadd_cn, user=user,all='True')
+     print "not extuser"
+    elif extuser:
+      client.hbacrule_add_user(emerg_hbacruleadd_cn, group=posixgroup_cn,all='True')
+      print "extuser true"
+      print posixgroup_cn
+
 
   # Call methods
-  sudoruleadd(user,servers)
-  hbacruleadd(user,servers)
-
-
+  if friendlyUserName:
+    posixgroup_cn = groupadd(friendlyUserName)
+  
+  sudoruleadd(user,servers,posixgroup_cn)
+  hbacruleadd(user,servers,posixgroup_cn)
  
 if __name__ == "__main__":
 #  print len(sys.argv)
 #  if len (sys.argv) != 5 :
 #    print "Error!\nUsage: ./emergencyAccess.py -u username -s server1,server2,server3"
 #    sys.exit (1)
-
+ 
   main(sys.argv[1:])
+ 
+# END OF SCRIPT #
